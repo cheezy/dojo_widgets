c4e2b36ad30442d14dafe95b3c97f7caa42547d0
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2013-07-20 15:02:51 -0400

    added start of Accordion widget

---
+++ features/step_definitions/dojo_demo_steps.rb
@@ -0,0 +1,11 @@
+Given(/^I am on the dojo demo page$/) do
+  visit DojoDemoPage
+end
+
+When(/^I select the "(.*?)" Accordion panel$/) do |label|
+  on(DojoDemoPage).the_accordion.select_panel label
+end
+
+Then(/^the Accordion panel for "(.*?)" should be visible$/) do |label|
+  on(DojoDemoPage).the_accordion.panel_for(label).should be_visible
+end
